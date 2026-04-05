bf794eb01a2cff8449fbb322917abec65f6d8705
SUNR153 <zariptursunov3@gmail.com>
2026-04-05 14:42:24 +0500

    fix: update emails.py

--- backend/candidates/emails.py
+++ backend/candidates/emails.py
@@ -1,23 +1,18 @@
-from mailersend import emails
+from mailersend import Email
 from django.conf import settings
 import logging
 
 logger = logging.getLogger(__name__)
 
+
 def _send(subject, message, recipient):
     try:
-        mailer = emails.NewEmail(settings.MAILERSEND_API_KEY)
-        mail_body = {}
-        mail_from = {
-            "name": "inVisionU",
-            "email": settings.MAILERSEND_FROM_EMAIL,
-        }
-        recipients = [{"email": recipient}]
-        mailer.set_mail_from(mail_from, mail_body)
-        mailer.set_mail_to(recipients, mail_body)
-        mailer.set_subject(subject, mail_body)
-        mailer.set_plaintext_content(message, mail_body)
-        mailer.send(mail_body)
+        mailer = Email(api_key=settings.MAILERSEND_API_KEY)
+        mailer.set_mail_from({"email": settings.MAILERSEND_FROM_EMAIL, "name": "inVisionU"})
+        mailer.set_mail_to([{"email": recipient}])
+        mailer.set_subject(subject)
+        mailer.set_plaintext_content(message)
+        mailer.send()
         logger.info(f"EMAIL SENT: {subject} → {recipient}")
     except Exception as e:
         logger.error(f"EMAIL ERROR: {subject} → {recipient} | {e}")
